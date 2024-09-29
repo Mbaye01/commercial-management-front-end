651bab22f13936082c57b5e704279419ebd8c449
mbayedeweb <ibzembaye@gmail.com>
2024-09-29 17:53:09 +0000

    updated main js an index js

--- src/Router/index.js
+++ src/Router/index.js
@@ -7,17 +7,18 @@ import EditOrder from "../components/orders/EditOrder.vue";
 import ShowOrder from "../components/orders/ShowOrder.vue";
 
 const routes = [
-  { path: "/", component: CustomersView },
-  { path: "/customers", component: CustomersView },
-  { path: "/products", component: ProductsView },
-  { path: "/orders", name: "orders", component: OrdersView },
-  { path: "/create", name: "create", component: AddOrder },
-  { path: "/edit/:id", name: "edit", component: EditOrder },
-  { path: "/view/id", name: "view", component: ShowOrder },
+  { path: "/", component: CustomersView }, // Page d'accueil sur la vue des clients
+  { path: "/customers", component: CustomersView }, // Route vers la vue des clients
+  { path: "/products", component: ProductsView }, // Route vers la vue des produits
+  { path: "/orders", name: "orders", component: OrdersView }, // Route vers la vue des commandes
+  { path: "/create", name: "create-order", component: AddOrder }, // Route pour ajouter une commande
+  { path: "/edit/:id", name: "edit-order", component: EditOrder }, // Route pour modifier une commande spécifique
+  { path: "/view/:id", name: "view-order", component: ShowOrder }, // Route pour visualiser une commande spécifique
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
 });
+
 export default router;
