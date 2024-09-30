ce398f36a9e52b558c3d3c316b077096c994b3a2
mbayedeweb <ibzembaye@gmail.com>
2024-09-30 12:37:44 +0000

    update main

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
 
 import { createApp } from "vue";
 import App from "./App.vue";
-import router from "./router";
+import router from "./Router/index.js";
 
 // Import Bootstrap styles and scripts
 import "bootstrap/dist/css/bootstrap.min.css";
